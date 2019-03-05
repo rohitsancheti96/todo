8e83af281e6621bbec659cb0824fb1e70d463366
rohitsancheti96 <rohitsancheti96@gmail.com>
2019-03-05 12:02:24 +0530

    Updates

---
+++ precache-manifest.dc797776f96f6f96dbd846d7f0083cee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "f5be15623527c09e24f1",
+    "url": "/todo/static/js/main.f5be1562.chunk.js"
+  },
+  {
+    "revision": "a75237ea0ef595a14061",
+    "url": "/todo/static/js/2.a75237ea.chunk.js"
+  },
+  {
+    "revision": "f5be15623527c09e24f1",
+    "url": "/todo/static/css/main.ed7b789f.chunk.css"
+  },
+  {
+    "revision": "82d7449b2b93748b62e16baf4981a5e5",
+    "url": "/todo/index.html"
+  }
+];
